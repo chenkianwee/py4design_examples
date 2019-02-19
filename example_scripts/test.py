b4d4f39ec0202660bb73369db0efcdddd2517fa8
chenkianwee <chenkianwee@users.noreply.github.com>
2019-02-19 14:35:19 -0500

    pt com

--- example_scripts/test.py
+++ example_scripts/test.py
@@ -1,4 +1,19 @@
-from dateutil.parser import parse
+import paramiko
 
-date = parse("2018-06-29-08:15:27.243860+08:00")
-print date
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.WarningPolicy())
+print("*** Connecting...")
+
+client.connect('chaosbox.princeton.edu',username="chaos", password='Zer0exergy')
+"""
+stdin, stdout, stderr = client.exec_command('ls')
+for line in stdout:
+    print('... ' + line.strip('\n'))
+
+
+"""
+ftp_client=client.open_sftp()
+dest_filepath = 'F:\\kianwee_work\\princeton\\2019_01_to_2019_06\\coldtube\\data\\csv\\chaosbox\\coldtube_ct_ph19_surfacet.csv'
+ftp_client.get('/home/chaos/coldtube_ct_ph19_surfacet.csv',dest_filepath)
+ftp_client.close()
+client.close()
